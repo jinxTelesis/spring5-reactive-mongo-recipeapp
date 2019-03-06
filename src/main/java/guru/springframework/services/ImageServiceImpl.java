ff4ee9f82be62be5274cefdc332609acf4e99e10
Dre <dremon7@hotmail.com>
2019-03-06 03:37:04 -0500

    basic refactor to compile again with reactive container

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -31,7 +31,31 @@ public class ImageServiceImpl implements ImageService {
     public Mono<Void> saveImageFile(String recipeId, MultipartFile file) {
 
 
-        
+        Mono<Recipe> recipeMono = recipeReactiveRepository.findById(recipeId)
+                .map(recipe -> {
+                    Byte[] byteObjects = new Byte[0];
+
+                    try{
+                        byteObjects = new Byte[file.getBytes().length];
+
+                        int i = 0;
+
+                        for(byte b : file.getBytes()){
+                            byteObjects[i++] = b;
+                        }
+
+                        recipe.setImage(byteObjects);
+
+                        return recipe;
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        throw new RuntimeException(e);
+                    }
+                });
+
+        recipeReactiveRepository.save(recipeMono.block()).block();
 
         return Mono.empty();
     }
